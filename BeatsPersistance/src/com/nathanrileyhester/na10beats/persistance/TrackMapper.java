f411b77423a43dd0e06196018321b65376f197f2
Nakanz <nathan.hester@gmail.com>
2019-06-25 16:07:13 -1000

    updated trackmapper to something more than itself

--- BeatsPersistance/src/com/nathanrileyhester/na10beats/persistance/TrackMapper.java
+++ BeatsPersistance/src/com/nathanrileyhester/na10beats/persistance/TrackMapper.java
@@ -1,5 +1,34 @@
 package com.nathanrileyhester.na10beats.persistance;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.nathanrileyhester.na10beats.domain.Track;
+
 public class TrackMapper {
+	private ArrayList<Track> queryDB(Connection con) {
+		ArrayList<Track> eList = new ArrayList<Track>();
+
+		try {
+			// step3 create the statement object
+			Statement stmt = con.createStatement();
+
+			// step4 execute query
+			ResultSet rs = stmt.executeQuery("select Tname, Ptrack, Author, Tlikes from TRACK;");
+			//Tname varchar(42),Ptrack blob,Author varchar(42),Tlikes INTEGER
+			while (rs.next()) {
+				Track tk = new Track();
+				
+				tk.setName(rs.getString(2));
+			
+				eList.add(tk);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return eList;
+	}
 
 }
